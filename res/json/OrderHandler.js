911da122c9cd9f32bc0581412c3ec8a05849dcf7
SamuelMoller <per.samuel.moller@gmail.com>
2024-02-27 10:18:56 +0100

    Separated Order() and OrderHandler()

--- res/json/OrderHandler.js
+++ res/json/OrderHandler.js
@@ -6,11 +6,50 @@
 // method that prints all beers from the JSON database file.
 //
 // TODO: removeFromOrder() method
+// =====================================================================================================
 
+class Order {
+    constructor() {
+        this.items = new Map(); // Create a map to store items in the order
+        console.log("Order() created");
+    }
+
+    addItem(item) {
+        let self = this;
+        if (self.items.has(item)) { self.items.set(item, self.items.get(item) + 1); // Increment item count if it exists
+        } else { self.items.set(item, 1); } // Add item to map if it doesn't exist, set count to 1
+        return self.items.get(item);
+    }
+
+    removeItem(item) {
+        let self = this;
+        if (self.items.has(item)) { self.items.set(item, self.items.get(item) - 1); // Decrement item count if it exists
+        } else { console.log("Item not in order"); } // Print error message if item doesn't exist
+        return self.items.get(item);
+    }
+
+    printOrder() {
+        let self = this;
+        let total = 0;
+        self.items.forEach(function(value, key) { // Iterate over items in order
+            if (value > 0) {
+                console.log(key + ": " + value); // Print item and count (should be replaced with model method call)
+                // self.items.set(key, 0); // Reset item count
+                total += value;
+            }
+        });
+        if (total > 0) { console.log("Total: " + total); 
+        } else { console.log("No items in order") }
+        if (total > 20) { console.log("Damn, bro"); } // im thoisty
+    }
+}
+
+// =====================================================================================================
 class OrderHandler {
     constructor() {
         this.items = new Map(); // Create a map to store items in the order
-        console.log("OrderHandler created");
+        this.buildOrderMenu(); // Call buildOrderMenu() when object is created
+        console.log("OrderHandler() created");
     }
 
 // =====================================================================================================
@@ -26,51 +65,21 @@ class OrderHandler {
 // =====================================================================================================
     listItems() {
         let self = this;
+        let order = new Order();
         $("#orderNow").off("click"); // Remove click event listener (do once)
         $("#orderMenu").append("<div id='beverages'></div>"); // Append div element to body
         $.getJSON("res/json/Beverages.js", function(data) { // Load JSON database
             $.each(data, function(key, val) { // Iterate over entries
                 $('<div id=container' + val.nr + ' class=beverageItemContainer>').on("click", function() { // Create div element with click event listener and unique id
-                    self.addToOrder(val.name, val.nr);
+                    order.addItem(val.name); // Add item to order
                 }).appendTo("#beverages"); // Append two paragraphs to the div element
                 $("<p class=beverageItem_L>").text(val.name).appendTo("#container" + val.nr); // Item name
                 $("<p id=bev" + val.nr + " class=beverageItem_R>").text("(0)").appendTo("#container" + val.nr); // Item count (space reserved)
             });
         });
         $("<button id=placeOrder>").text("Place order").on("click", function() {
-            self.printOrder();
+            order.printOrder();
         }).appendTo("#orderMenu");
-    }
-
-// =====================================================================================================
-    addToOrder(item, id) {
-        let self = this;
-        if (self.items.has(item)) { self.items.set(item, self.items.get(item) + 1); // Increment item count if it exists
-        } else { self.items.set(item, 1); } // Add item to map if it doesn't exist, set count to 1
-        $("#bev" + id).text("(" + self.items.get(item) + ")"); // Update view item count
-        $("#bev" + id).css("visibility", "visible"); // Make item count visible
-    }
-
-// =====================================================================================================
-// 
-    printOrder() {
-        let self = this;
-        let total = 0;
-        self.items.forEach(function(value, key) { // Iterate over items in order
-            if (value > 0) {
-                console.log(key + ": " + value); // Print item and count (should be replaced with model method call)
-                self.items.set(key, 0); // Reset item count
-                total += value;
-            }
-        });
-        if (total > 0) { console.log("Total: " + total); 
-        } else { console.log("No items in order") }
-        if (total > 20) { console.log("Damn, bro"); } // im thoisty
-        $("#beverages").remove(); // Remove order menu
-        $("#placeOrder").remove(); // Remove place order button
-        $("#orderNow").on("click", function(){ // Re-enable order menu
-            self.listItems();
-        });
     }
 
 // =====================================================================================================
