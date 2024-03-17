8ed7f823a195a8e2238bf591e9b3c405808efeea
SamuelMoller <per.samuel.moller@gmail.com>
2024-03-17 17:19:36 +0100

    A QoL header change

--- res/json/pages/Header.js
+++ res/json/pages/Header.js
@@ -15,11 +15,15 @@ export class Header {
         $("header").append("<div class='header-content'></div>");
         $(".header-content").append("<div id='header-content-left'></div>");
         $(".header-content").append("<div id='header-content-right'></div>");
-        $("#header-content-left").append("<img src='res/img/logo-small.png' >");
-        $("#header-content-left").append("<h1>The Old Sea Turtle</h1>");
+        $("#header-content-left").append("<img id='header-img' src='res/img/logo-small.png' >");
+        $("#header-content-left").append("<h1 id='header-title'>The Old Sea Turtle</h1>");
         $("#header-content-right").append("<p id='headerBooking'>Book a table</p>");
         $("#header-content-right").append("<p id='headerApply'>Apply for VIP</p>");
         $("#header-content-right").append("<p id='headerLogin'>Login</p>");
+
+        $("#header-img, #header-title").on("click", function() {
+            PH.page("order");
+        });
 
         $("#headerLogin").on("click", function() {
             PH.page("login");
