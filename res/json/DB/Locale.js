13ace479a4b128edaea2f508709b94b2478751ec
SamuelMoller <per.samuel.moller@gmail.com>
2024-03-19 13:17:06 +0100

    Prototyping a locale database

---
+++ res/json/DB/Locale.js
@@ -0,0 +1,31 @@
+export var Locale = {
+    "en-US": [
+        {
+            "submit": "Submit",
+            "undo": "Undo",
+            "redo": "Redo",
+            "addNewItem": "Add new item",
+        }],
+        "sv-SE": [
+        {
+            "submit": "Skicka",
+            "undo": "Ångra",
+            "redo": "Gör om",
+            "addNewItem": "Lägg till ny vara",
+        }],
+        "es-ES": [
+        {
+            "submit": "Enviar",
+            "undo": "Deshacer",
+            "redo": "Rehacer",
+            "addNewItem": "Añadir nuevo artículo",
+        }],
+        "fr-FR": [
+            {
+            "submit": "Soumettre",
+            "undo": "Annuler",
+            "redo": "Refaire",
+            "addNewItem": "Ajouter un nouvel article",
+        }
+    ]
+};
